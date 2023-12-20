1bd91adf79b219d80d1f53597daf872e3593f8d1
alenko998 <alen_git@hotmail.com>
2023-12-20 17:57:24 +0100

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { AboutComponent } from './page/about/about.component';
+import { TeamComponent } from './page/team/team.component';
+import { ContactComponent } from './page/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'team', component:TeamComponent},
+  {path: 'contact', component:ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './page/home/home.component';
+import { AboutComponent } from './page/about/about.component';
+import { TeamComponent } from './page/team/team.component';
+import { ContactComponent } from './page/contact/contact.component';
+import { HeroComponent } from './component/hero/hero.component';
+import { SliderComponent } from './component/slider/slider.component';
+import { AboutCompanyComponent } from './component/about-company/about-company.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    TeamComponent,
+    ContactComponent,
+    HeroComponent,
+    SliderComponent,
+    AboutCompanyComponent
   ],
   imports: [
     BrowserModule,
